827b6ee60cfe5f1dc9ba094b0c1690c4a134d6f9
Balloon <Podginator@gmail.com>
2014-08-16 03:27:22 +0100

    New functions

--- js/main.js
+++ js/main.js
@@ -22,9 +22,78 @@ function DoClip(div, x, y, size){
 
 function HideAll(div){
 	$(div).each(function(){
-		console.log(this)
 		$(this).hide();
 	});
+}
+
+function RoundToTen(num){
+	return Math.round(num/10)*10;
+}
+
+function GetMode(data){
+	var modeMap = {};
+	var maxEl,
+	maxCount = 1,
+	i = 1;
+	while (data[i++] != null){
+		var colour = data[i];
+		if (modeMap[colour]!=null)
+		{
+			modeMap[colour] += 1
+		}else{
+			modeMap[colour] = 1
+		}
+
+		if(modeMap[colour] > maxCount && colour != 0){
+			maxCount = colour
+		}
+	}
+
+	console.log(maxCount)
+	return maxCount;
+}
+
+function GetAverage(data){
+	var i = 1,
+	count = 0,
+	total = 0
+	while(data[i++] != null){
+		if(data[i] != 0 && data[i] != undefined){
+			total += data[i]
+			++count
+		}
+	}
+	return Math.floor(total/count);
+
+}
+
+function stringToHex(s){
+	var hex = s.toString(16);
+	return hex.length == 1? "0" + hex : hex;
+}
+
+function RGBToHex(r,g,b){
+	return "#" + stringToHex(r) + stringToHex(g) + stringToHex(b)
+}
+
+function GetDominantColour(image){
+
+	var canvas = document.createElement('canvas'),
+		context = canvas.getContext('2d');
+
+	var rgb = {r:{},g:{},b:{}}
+	context.drawImage(image, 0,0,image.width, image.height)
+	var img = context.getImageData(0,0,image.width,image.height)
+	var i = count = 0;
+
+ 	while ( (i += 5 * 4) < img.data.length ) {
+        count++
+        rgb.r[count] = img.data[i]>50 ? RoundToTen(img.data[i]) : 0;
+        rgb.g[count] = img.data[i]>50 ? RoundToTen(img.data[i+1]) : 0;
+        rgb.b[count] = img.data[i]>50 ? RoundToTen(img.data[i+2]) : 0;
+    }
+
+    return RGBToHex(GetAverage(rgb.r), GetAverage(rgb.g), GetAverage(rgb.b));
 }
 
 function OnScreen(div){
@@ -34,15 +103,14 @@ function OnScreen(div){
 	var divTop = $(div+":first").offset().top;
 	var divBottom = $(div).height() + divTop
 
-	console.log(divTop<=windowBottom, divTop, windowBottom)
 	return ((divTop<= windowBottom));
 }
 
 function ScrollCheck() {
-		    if(OnScreen('.worktile') && $('.worktile').css('opacity') == 0){
-		    	toggleContainers('.worktile')
-		    }
-		}
+    if(OnScreen('.worktile') && $('.worktile').css('opacity') == 0){
+    	toggleContainers('.worktile')
+    }
+}
 
 $( document ).ready(function() {
 		$('#Welcome').Banner();
@@ -72,8 +140,6 @@ $( document ).ready(function() {
 			$('.content .'+header).show();
 		});
 
-		var didScroll = false;
-
 		$(window).scroll(function(){
 			ScrollCheck();
 
@@ -89,7 +155,15 @@ $( document ).ready(function() {
 		}) 
 
 		$('nav a').click(function(){
-			$('html, body').stop().animate({scrollTop:$("#"+$(this).attr('href')).offset().top -40},2000);
+			$('html, body').stop().animate({
+				scrollTop:$("#"+$(this).attr('href')).offset().top -40
+			},1000);
 			return false;
+		});
+
+		$('.worktile img').click(function(){
+			var overlay = $(this).parent().find(".overlay")
+			$(this).parent().find(".overlay").css({background: GetDominantColour(overlay), zIndex:200})
+			DoClip(overlay, this.height/2, this.width/2, 200);
 		});
 });
